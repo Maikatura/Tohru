b0cc88b6302527e4eb27cc881df275b8ab3c7490
Zyro / Maikatura <31046190+Maikatura@users.noreply.github.com>
2022-11-12 16:39:42 +0100

    :D

---
+++ commands/Help/info.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+  name: "info",
+  description: "Some info about me",
+  usage: "",
+  aliases: ["path"],
+
+  execute: async (client, interaction, args) => {
+
+    await interaction.reply({
+        embeds:
+          [
+            await CreateEmbed()
+          ]
+      });
+
+  },
+  SlashCommand: {
+
+    execute: async (client, interaction, args) => {
+
+        let embed = new EmbedBuilder();
+
+        interaction.reply({
+            embeds:
+              [
+                await CreateEmbed()
+              ]
+          });
+
+
+    }
+  }
+  
+}
+
+function CreateEmbed()
+{
+    embed = new EmbedBuilder();
+
+    let message = `I was made by someone called Maikatura!\n`;
+    message += `I can be found [here](https://github.com/Maikatura/Tohru)\n`
+    message += `Read bot ToS [here](https://github.com/Maikatura/Tohru)!`
+
+    embed.setDescription(`I was made by someone called Maikatura!\nI can be found [here](https://github.com/Maikatura/Tohru)`);
+
+    return embed;
+}
---
+++ docs/tos.md
@@ -0,0 +1,9 @@
+# Terms of Service
+
+la la la I dont came I dont have a terms of service just want to clearify.
+
+1. Bot does not collect data like, users, messages or any type of data (look through the bot and see for yourself).
+2. I only use data that is needed for the bot to function. (Its deleted after the task is complete).
+3. My bot is 100% free and you are allowed to self host it but please dont remove the info command :)
+4. I provide my bot for free at no additional cost. This mean support me on github or star the repo if you like the bot.
+5. Have fun listening to music for free.
